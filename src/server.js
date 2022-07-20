f442854b7666df510c5462694bb8c73a7a7e39c8
LeandroPS <leandro.pires.souza@gmail.com>
2022-07-19 23:54:06 -0300

    WIP

--- src/server.js
+++ src/server.js
@@ -1,20 +1,29 @@
 const { setEventHandlers } = require("./events.js");
-const { Server } = require("socket.io");
+const server = require("http").createServer();
+
+const io = require("socket.io")(server, {
+  //path: "/test",
+  //serveClient: false,
+  // below are engine.IO options
+  pingInterval: 10000,
+  pingTimeout: 5000,
+  cookie: false,
+
+  transports: ["websocket"],
+});
 
 const init = () => {
-  const server = new Server();
-
-  const serverWithHandlers = setEventHandlers(server);
+  const serverWithHandlers = setEventHandlers(io);
 
   serverWithHandlers.listen(3000);
 
-  serverWithHandlers.on("connection", (socket) => {
-    console.log("novo client");
-    socket.on("planning", (data) => {
-      console.log(data);
-      if (data == "ola!") serverWithHandlers.emit("planning", "como vai?");
-    });
-  });
+  // serverWithHandlers.on("connection", (socket) => {
+  //   console.log("novo client");
+  //   socket.on("planning", (data) => {
+  //     console.log(data);
+  //     if (data == "ola!") serverWithHandlers.emit("planning", "como vai?");
+  //   });
+  // });
 };
 
 module.exports = { init };
